ec72e21d47d074d3dc1fb71d0dd83f62d15f6bad
Aditya Ramteke <adityaramteke05icr@gmail.com>
2018-02-12 19:35:00 +0530

    Created ball

--- paddleball.py
+++ paddleball.py
@@ -1,8 +1,21 @@
 from tkinter import *
+import time
+import random
+
+class Ball:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
+        self.canvas.move(self.id, 245, 100)
+
+
 tk = Tk()
 tk.title("Bounce")
 tk.resizable(0, 0)
 tk.wm_attributes("-topmost", 1)
-canvas = Canvas(tk, width=400, height=400, bd=0, highlightthickness=0)
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
+tk.update()
+
+ball = Ball(canvas, 'green')
 tk.mainloop()
