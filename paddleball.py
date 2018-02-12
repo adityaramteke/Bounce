5e173f4f49158dee4e764abf4d34e464ffb9ccac
Aditya Ramteke <adityaramteke05icr@gmail.com>
2018-02-12 21:40:29 +0530

    making the ball bounce.

--- paddleball.py
+++ paddleball.py
@@ -7,6 +7,17 @@ class Ball:
         self.canvas = canvas
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
+        self.x = 0
+        self.y = -1
+        self.canvas_height = self.canvas.winfo_height()
+
+    def move(self):
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <= 0:
+            self.y = 1
+        if pos[3] >= self.canvas_height:
+            self.y = -1
 
 
 tk = Tk()
@@ -18,4 +29,11 @@ canvas.pack()
 tk.update()
 
 ball = Ball(canvas, 'green')
+
+while 1:
+        ball.move()
+        tk.update_idletasks()
+        tk.update()
+        time.sleep(0.01)
+
 tk.mainloop()
